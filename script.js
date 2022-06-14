5f44c513a1c0cd9eab34bec475a0005a51c7236e
eb <eiler.byberi@gmail.com>
2022-06-13 19:10:28 -0700

    Add the operation functions and the operate function

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function operate(operator, a, b) {
+    if (operator === 'add') {
+        add(a, b);
+    } else if (operator === 'subtract'){
+        subtract(a, b);
+    } else if (operator === 'multiply') {
+        multiply(a, b);
+    } else if (operator === 'divide') {
+        divide(a, b);
+        }
+}
