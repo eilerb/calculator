4890ebd250d1e9f70984927387b8d55bbd9497b8
eb <eiler.byberi@gmail.com>
2022-06-15 20:38:53 -0700

    Add number button functionality and start working on the clear btn

--- script.js
+++ script.js
@@ -25,3 +25,36 @@ function operate(operator, a, b) {
         divide(a, b);
         }
 }
+
+const display = document.querySelector('.display');
+let displayText = '';
+//console.log(display.textContent);
+//display.textContent = '1';
+//console.log(display.textContent);
+console.log(displayText);
+
+// Number button functionality
+const buttons = document.querySelectorAll('.number');
+buttonsArray = [...buttons];
+let numberLength = 0;
+console.log(buttonsArray);
+buttonsArray.forEach(button => {
+    button.addEventListener('click', () => {
+        if (numberLength < 10) {
+            display.textContent += button.textContent;
+        }
+        
+        numberLength++;
+        displayText = display.textContent;
+        //console.log(displayText);
+    })
+})
+
+
+// Clear button
+const clearBtn = document.querySelector('.clear');
+clearBtn.addEventListener('click', () => {
+    displayText.slice(displayText.length-1);
+    console.log(displayText);
+    display.textContent = displayText;
+});
